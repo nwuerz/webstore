fa3b741300f842359af9404daa9bc30e43e6dff4
Colby Norman <colbynorm41@gmail.com>
2020-03-31 21:12:47 -0500

    changed some files to run eslint

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-var Sequelize = require('sequelize');
+var Sequelize = require("sequelize");
 
 // Create mySQL connection
-var sequelize = new Sequelize('zombie_db', 'root', '', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
+var sequelize = new Sequelize("zombie_db", "root", "", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
