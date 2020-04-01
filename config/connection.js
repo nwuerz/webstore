1f213552e5ffcc6a79df30ca34522bf297571c86
Colby Norman <colbynorm41@gmail.com>
2020-03-31 20:50:40 -0500

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-var Sequelize = require("sequelize");
+var Sequelize = require('sequelize');
 
 // Create mySQL connection
-var sequelize = new Sequelize("zombie_db", "root", "", {
-    host: "localhost",
+var sequelize = new Sequelize('zombie_db', 'root', '', {
+    host: 'localhost',
     port: 3306,
-    dialect: "mysql",
+    dialect: 'mysql',
     pool: {
         max: 5,
         min: 0,
