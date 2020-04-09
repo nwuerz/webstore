289a6a55ef81ac5eb9e66fefd2138414c7f71613
nick wuerz <nawuerz@gmail.com>
2020-04-09 11:45:01 -0500

    styled toolbar on categories page(s)

--- public/js/category.js
+++ public/js/category.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-  $(".fixed-action-btn").floatingActionButton();
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {
+    toolbarEnabled: true
+  });
 });
